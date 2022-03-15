03c12cb6cd92caf30da21edb9fe2e2056fde833e
Nikita Ivanov <cheloveknikitaa@gmail.com>
2022-03-15 14:09:27 +0300

    final

--- image.sh
+++ image.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-if [ "$#" -lt 1 ] ; then
+if [ $# -lt 1 ] ; then
     files=$(find ./ -iname '*.jpg' -and \! -iname '*_thumbnail.jpg')
 else
-    files=$(cat $1)
+    files=$(grep '.jpg' $@ | grep -v '_thumbnail.jpg')
 fi
 
 for file in $files ;
 do
-    
-    width=$(identify -format "%w" $file)
-    height=$(identify -format "%h" $file)
-    if [ $width -gt $height ] ; then
-        echo "width"
-        convert $file -resize 320! ${file/.jpg/_thumbnail.jpg}
-    else
-        echo "height"
-        convert $file -resize x320! ${file/.jpg/_thumbnail.jpg}
+    if [ -f $file ]; then
+        width=$(identify -format "%w" $file)
+        height=$(identify -format "%h" $file)
+        if [ $width -gt $height ] ; then
+            convert $file -resize 320! ${file/.jpg/_thumbnail.jpg}
+        else
+            convert $file -resize x320! ${file/.jpg/_thumbnail.jpg}
+        fi
     fi
 done
-
-
-# for file in *.jpg ; do convert $file -resize 830 $file; done
-# find ./ -iname '*.jpg' -and \! -iname '*_thumbnail.jpg' -exec convert \{} -resize 320\> \{} \;
